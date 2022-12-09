52ee86d211893011b82b77a477944421b178ca7f
Zac Bir <zbir@zacbir.net>
2022-12-09 17:31:52 -0500

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,53 @@
+AUTHOR = 'Zac Bir'
+SITENAME = 'Neverending Pretending'
+SITEURL = 'http://localhost:8080'
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+MENUITEMS = (
+    ('Mastodon', 'https://dice.camp/@zacbir'),
+    ('Itch.io', 'https://zacbir.itch.io'),
+    ('Archives', '/archives.html')
+)
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+THEME = 'Peli-Kiera'
+
+# PLUGIN_PATHS = ['/Users/zbir/Dev/zacbir.net/pelican-plugins']
+PLUGINS = ['liquid_tags', 'search', 'neighbors']
+
+LIQUID_TAGS = ["img"]
+
+DISPLAY_CATEGORIES_ON_MENU = False
+
+STATIC_PATHS = ['images', 'static', '.well-known']
+
+EXTRA_HEADER = '<link rel="me" href="https://dice.camp/@zacbir">'
+
+COPYRIGHT = '2013-2022'
+
+CUSTOM_CSS_FILE = 'static/css/custom.css'
